a9da69ce79e43a0bd2c012d591f02fb281ec8d96
SaraIversen <sara.skovgaard.iversen@gmail.com>
2025-11-27 12:50:22 +0100

    get metoder til humidity, temperature og light

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -9,8 +9,8 @@ const HomePage = {
 
             <h3>Data for støjniveau:</h3>
 
-            <div v-if="error" style="color: red">Error: {{error}}</div>
-            <div v-if="statuscode" style="color: green">Statuscode: {{statuscode}}</div>
+            <div v-if="errorNoise" style="color: red">Error: {{errorNoise}}</div>
+            <div v-if="statuscodeNoise" style="color: green">Statuscode: {{statuscodeNoise}}</div>
 
             <ul v-if="noisesList.length">
                 <li v-for="noise in noisesList" :key="noise.id">
@@ -25,7 +25,18 @@ const HomePage = {
             <br>
 
             <h3>Data for rumtemperatur:</h3>
-            <p>Kommer snart :-)</p>
+
+            <div v-if="errorHumidities" style="color: red">Error: {{errorHumidities}}</div>
+            <div v-if="statuscodeHumidities" style="color: green">Statuscode: {{statuscodeHumidities}}</div>
+
+            <ul v-if="humiditiesList.length">
+                <li v-for="humidity in humiditiesList" :key="humidity.id">
+                    Indhentet Humidity [Id: {{humidity.id}}], 
+                    [Decibel: {{humidity.decibel}}], 
+                    [Dato: {{ new Date(humidity.time).toLocaleDateString() }}], 
+                    [Tidspunkt: {{ new Date(humidity.time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) }}]
+                </li>
+            </ul>
 
             <br>
             <br>
@@ -52,26 +63,81 @@ const HomePage = {
             image: './assets/images/stress- og fokusmonitor for studerende.png',
 
             noisesList: [],
-            error: null, 
-            statuscode: null,
+            humiditiesList: [],
+            temperaturesList: [],
+            lightsList: [],
+
+            errorNoise: null, 
+            statuscodeNoise: null,
+
+            errorHumidities: null, 
+            statuscodeHumidities: null,
+
+            errorTemperature: null, 
+            statuscodeTemperature: null,
+
+            errorLight: null, 
+            statuscodeLight: null,
         }
     },
     // created() is called automatically when the page is loaded.
     async created() {
         console.log("created method called");
         await this.getAllNoises();
+        await this.getAllHumidities();
+        await this.getAllTemperatures();
+        await this.getAllLights();
     },
     methods: {
         async getAllNoises() {
             this.error = null;
 
-            await axios.get(baseUri)
+            await axios.get(baseUriNoise)
             .then(response => {
                 this.noisesList = response.data;
                 this.statuscode = response.status;
             })
             .catch(error => {
                 this.noisesList = [];
+                this.error = error.message;
+            })  
+        },
+        async getAllHumidities() {
+            this.error = null;
+
+            await axios.get(baseUriHumidity)
+            .then(response => {
+                this.humiditiesList = response.data;
+                this.statuscode = response.status;
+            })
+            .catch(error => {
+                this.humiditiesList = [];
+                this.error = error.message;
+            })  
+        },
+        async getAllTemperatures() {
+            this.error = null;
+
+            await axios.get(baseUriTemperature)
+            .then(response => {
+                this.temperaturesList = response.data;
+                this.statuscode = response.status;
+            })
+            .catch(error => {
+                this.temperaturesList = [];
+                this.error = error.message;
+            })  
+        },
+        async getAllLights() {
+            this.error = null;
+
+            await axios.get(baseUriLight)
+            .then(response => {
+                this.lightsList = response.data;
+                this.statuscode = response.status;
+            })
+            .catch(error => {
+                this.lightsList = [];
                 this.error = error.message;
             })  
         }

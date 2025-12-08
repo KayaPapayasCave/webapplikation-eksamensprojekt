ebe11c473bc875bc02d9c84faa5486ee697f89e5
KayaPapaya <kthorsen2004@gmail.com>
2025-12-08 11:42:25 +0100

    Selenium tilføjet til projektet, der er kommet en masse filer med lol. Der er lavet Selenium tests for links, pages og api. Når vi får mulighed for at kunne få data fra Raspberry Pi'en ind på webapplikationen, så skal der laves nogle tests for det også.

---
+++ selenium-tests/test-links.js
@@ -0,0 +1,25 @@
+// For at teste skal dette køres i terminal: node selenium-tests/test-links.js
+// Hvis den virker ser vi "Links er fundet" i konsollen.
+// Den er kun lavet for Google Chrome-browseren.
+
+const { Builder, By } = require('selenium-webdriver');
+
+(async () => {
+  let driver = await new Builder().forBrowser("chrome").build();
+
+  try {
+    // Tester om links findes
+    await driver.get("http://127.0.0.1:5501/#/");
+
+    const home = await driver.findElement(By.linkText("Home"));
+    const axios = await driver.findElement(By.linkText("Axios Test"));
+    const api = await driver.findElement(By.linkText("API Test"));
+
+    console.log("Links er fundet");
+
+  } catch (err) {
+    console.error("Fejl i testen:", err);
+  } finally {
+    await driver.quit();
+  }
+})();
